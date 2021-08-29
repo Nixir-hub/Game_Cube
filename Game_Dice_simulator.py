cf48cc4f46f066bbd2d51cf8e92ae9fe3b10067f
Nixir-hub <nixir@hotmail.com>
2021-08-29 11:14:09 +0200

    2 version

--- Game_Dice_simulator.py
+++ Game_Dice_simulator.py
@@ -7,17 +7,21 @@ def dice(y):
     :return: number of dice
     """
     dice_y = [3, 4, 6, 8, 10, 12, 20, 100]
-    if y in dice_y:
-        return y
-
+    try:
+        if y in dice_y:
+            return y
+    except:
+        return ValueError
 def throw_dice(y):
     """
 
     :param y dice walls
     :return: number  of dice wall
     """
-    return random.choice((list(range(1, dice(y)))))
-
+    try:
+        return random.choice((list(range(1, dice(y)))))
+    except:
+        return ValueError
 def game_dice_sum(z=0):
     """
 
@@ -26,9 +30,11 @@ def game_dice_sum(z=0):
     """
     while True:
         try:
+            print("Dices: d3, d4, d6, d8, d10, d12, d20, d100 ")
             x = int(input("Chose throw number: "))
             y = int(input("Chose dice type: "))
             z = int(input("Chose extra modifer: "))
+            dice_y = [3, 4, 6, 8, 10, 12, 20, 100]
             all_dice_throws = [throw_dice(y) for x in range(x)]
             if type(x) == ValueError:
                 break
@@ -36,6 +42,8 @@ def game_dice_sum(z=0):
                 break
             elif type(z) == ValueError:
                 break
+            elif y not in dice_y:
+                raise ValueError
             elif x < 1:
                 print("If you want to throw u must set >0 in throw number ")
             elif y < 3:
@@ -43,7 +51,7 @@ def game_dice_sum(z=0):
             else:
                 break
         except Exception:
-            print("All parms must be numbers!")
+                print("""All parms must be numbers!""")
     return f'Sum of throws is: {sum(all_dice_throws) + z}'
 
 print(game_dice_sum())
